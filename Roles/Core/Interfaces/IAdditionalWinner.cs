5deda491565ef5f84e3fbde3b4de01481d030471
tomorrow-CN <myy2118017530@163.com>
2025-06-08 15:42:23 +0800

    Revert "Initial commit: CrewmateOrImpostor mod"
    This reverts commit a2c6be6e4f20b3a2199445c187d0ec60138962ee.

--- Roles/Core/Interfaces/IAdditionalWinner.cs
+++
@@ -1,6 +0,0 @@
-namespace CrewmateOrImpostor.Roles.Core.Interfaces;
-
-public interface IAdditionalWinner
-{
-    public bool CheckWin(ref CustomRoles winnerRole);
-}
--- Roles/Core/Interfaces/IKillFlashSeeable.cs
+++
@@ -1,6 +0,0 @@
-namespace CrewmateOrImpostor.Roles.Core.Interfaces;
-
-public interface IKillFlashSeeable
-{
-    public bool CheckKillFlash(MurderInfo info) => true;
-}
